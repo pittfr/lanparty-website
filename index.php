2a5c5e1f69e6be02d76e15beda4faeade8dfca29
Pitt <dpittmor@gmail.com>
2025-05-31 23:46:54 +0100

    Renames navbar.css to header.css
    The file `navbar.css` has been renamed to `header.css` to
    better reflect its content and purpose within the project.
    Additionally, the padding-bottom for the header was adjusted.

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
     <title><?php echo ucfirst($page); ?></title>
     
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/footer.css">
     
     <?php foreach($pageSpecificCSS as $css): ?>
